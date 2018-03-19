c661e4401f9854e988b51ccbb882a573e0990ff1
Ruoyun Chen <chen5282@umn.edu>
2018-03-19 18:59:01 -0500

    starting iteration2

---
+++ iteration2/src/motion_handler_obstacle.cc
@@ -0,0 +1,77 @@
+/**
+ * @file motion_handler_obstacle.cc
+ *
+ * @copyright 2018 3081 Ruoyun, All rights reserved.
+ */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "src/motion_handler_obstacle.h"
+#include "src/motion_behavior_differential.h"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+NAMESPACE_BEGIN(csci3081);
+
+/*******************************************************************************
+ * Member Functions
+ ******************************************************************************/
+void MotionHandlerObstacle::IncreaseSpeed() {
+  set_velocity(
+    clamp_vel(get_velocity().left  + get_speed_delta()),
+    clamp_vel(get_velocity().right + get_speed_delta()));
+}
+
+void MotionHandlerObstacle::DecreaseSpeed() {
+  set_velocity(
+    clamp_vel(get_velocity().left  - get_speed_delta()),
+    clamp_vel(get_velocity().right - get_speed_delta()));
+}
+
+
+void MotionHandlerObstacle::TurnLeft() {
+  set_velocity(
+    clamp_vel(get_velocity().left  - get_angle_delta()),
+    clamp_vel(get_velocity().right + get_angle_delta()));
+}
+
+void MotionHandlerObstacle::TurnRight() {
+  set_velocity(
+    clamp_vel(get_velocity().left  + get_angle_delta()),
+    clamp_vel(get_velocity().right - get_angle_delta()));
+}
+
+double MotionHandlerObstacle::clamp_vel(double vel) {
+  double clamped = 0.0;
+  if (vel > 0) {
+    clamped = (vel > get_max_speed()) ?
+              get_max_speed():
+              vel;
+  } else {
+    clamped = (vel < 0) ?
+              0:
+              vel;
+  }
+  return clamped;
+} /* clamp_vel() */
+
+void MotionHandlerObstacle::UpdateVelocity() {
+  if (entity_->get_touch_sensor()->get_output()) {
+    turn_flag_ = true;
+    turn_step_ = 0;
+  }
+  // if obstacle collides with other objects, it will turn left 10 times
+  if (turn_flag_ && turn_step_< 10) {
+    TurnLeft();
+    turn_step_++;
+  } else {
+    // after it turns, it will go straight util the next collision
+    set_velocity(5, 5);
+    turn_flag_ = false;
+    turn_step_ = 0;
+  }
+}
+
+NAMESPACE_END(csci3081);
---
+++ iteration2/src/robot.cc
@@ -0,0 +1,105 @@
+/**
+ * @file robot.cc
+ *
+ * @copyright 2017 3081 Staff, All rights reserved.
+ */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "src/robot.h"
+#include "src/params.h"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+NAMESPACE_BEGIN(csci3081);
+
+/*******************************************************************************
+ * Constructors/Destructor
+ ******************************************************************************/
+Robot::Robot() :
+    motion_handler_(this),
+    motion_behavior_(this),
+    lives_(9),
+    time_count_(0),
+    mercy_flag_(false) {
+  set_type(kRobot);
+  set_color(ROBOT_COLOR);
+  set_pose(ROBOT_INIT_POS);
+  set_radius(ROBOT_RADIUS);
+}
+/*******************************************************************************
+ * Member Functions
+ ******************************************************************************/
+void Robot::TimestepUpdate(unsigned int dt) {
+  // Update heading as indicated by touch sensor
+  motion_handler_.UpdateVelocity();
+
+  // Use velocity and position to update position
+  motion_behavior_.UpdatePose(dt, motion_handler_.get_velocity());
+
+  // Reset Sensor for next cycle
+  sensor_touch_->Reset();
+
+  //  have robot flash in collision during 2 seconds
+  if (mercy_flag_ && time_count_ <20) {
+    time_count_++;
+    if (time_count_%2 ==0) {
+      set_color({255, 255, 0});
+    } else {
+      set_color(ROBOT_COLOR);
+    }
+  } else if (mercy_flag_ && time_count_ >=20) {
+    mercy_flag_ = false;
+    time_count_ = 0;
+    set_color(ROBOT_COLOR);
+  }
+} /* TimestepUpdate() */
+
+void Robot::Reset() {
+  set_color(ROBOT_COLOR);
+  set_pose({static_cast<double>((30 + (random() % 19) * 50)),
+        static_cast<double>((30 + (random() % 14) * 50))});
+  motion_handler_.set_max_speed(ROBOT_MAX_SPEED);
+  motion_handler_.set_max_angle(ROBOT_MAX_ANGLE);
+  lives_ = 9;
+  motion_handler_.set_velocity(0, 0);
+  sensor_touch_->Reset();
+} /* Reset() */
+
+void Robot::HandleCollision(EntityType object_type, ArenaEntity * object) {
+  // stop when collides
+  motion_handler_.set_velocity(0.0, 0.0);
+  if (object_type != kBase) {
+    // if collidsion, then mercy_flag_ is true for 2 seconds
+    //  and only lose life when mercy_flag_ is false
+    if (!mercy_flag_) {
+      lives_--;
+      mercy_flag_ = true;
+    }
+
+  } else if (object_type == kBase) {
+    Base* base_temp_ = dynamic_cast<Base*>(object);
+    object->set_color({255, 159, 0});
+    if (!base_temp_->IsCaptured()) {
+      base_temp_->set_captured(true);
+    }
+  }
+  sensor_touch_->HandleCollision(object_type, object);
+}
+
+void Robot::IncreaseSpeed() {
+  motion_handler_.IncreaseSpeed();
+}
+void Robot::DecreaseSpeed() {
+  motion_handler_.DecreaseSpeed();
+}
+void Robot::TurnRight() {
+  motion_handler_.TurnRight();
+}
+void Robot::TurnLeft() {
+  motion_handler_.TurnLeft();
+}
+
+NAMESPACE_END(csci3081);
